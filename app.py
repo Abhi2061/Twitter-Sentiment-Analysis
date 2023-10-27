52d9de2c60a2b59ae2a4697284c769c2045e07e4
Abhi2061 <abhishek2061a@gmail.com>
2023-10-27 21:34:41 +0530

    First Upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import re
+from textblob import TextBlob
+from flask import Flask, render_template , redirect, url_for, request
+        
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
+def home():
+  return render_template("index.html")
+
+@app.route("/predict", methods=['POST','GET'])
+def pred():
+	if request.method=='POST':
+            query=request.form['query']            
+
+            positive = negative = neutral = 0
+
+            # Iterate through the tweets in the dataset
+            for tweet in df['text']:
+                # Check if the search word/phrase is in the tweet
+                if query.lower() in tweet.lower():
+                    blob = TextBlob(tweet)
+                    sentiment = blob.sentiment
+                    polarity = sentiment.polarity
+
+                    # Determine sentiment based on the compound score
+                    if polarity > 0:
+                        positive += 1
+                    elif polarity < 0:
+                        negative += 1
+                    else:
+                        neutral += 1
+
+            res = []
+
+            res.append({'sentiment' : 'Positive Tweets','count' : positive});
+            res.append({'sentiment' : 'Negative Tweets','count' : negative});
+            res.append({'sentiment' : 'Neutral Tweets','count' : neutral});
+            
+            return render_template('result.html', result=res)
+
+if __name__ == '__main__': 
+
+    # Load your dataset into a pandas DataFrame
+    df = pd.read_csv('Twitter_Data.csv')
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+    
+    app.debug=True
+    app.run(host='localhost')
+
